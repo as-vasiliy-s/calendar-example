54ebd13a889368f4a3ead2e080e07f229b243086
Vasiliy Sukhachev <vasiliy.s.as@agiliumlabs.com>
2018-03-22 10:26:02 +0700

    main func

--- src/index.js
+++ src/index.js
@@ -9,17 +9,21 @@ import {createStore} from 'redux'
 import {calendarApp} from './reducer'
 import {Provider} from 'react-redux'
 
-let store = createStore(calendarApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export default function renderCalendar(elementId, defaultTimeStart = moment().startOf("day"), defaultTimeEnd = moment().endOf("day")) {
+    const store = createStore(calendarApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-const defaultTimeStart = moment().startOf("day");
-const defaultTimeEnd = moment().endOf("day");
+    render(
+        <Provider store={store}>
+            <App {...{defaultTimeStart, defaultTimeEnd}}/>
+        </Provider>, document.getElementById(elementId));
 
-// TODO: check https://github.com/fkhadra/react-on-screen
-// TODO: check https://github.com/joshwnj/react-visibility-sensor
+    registerServiceWorker();
+}
 
-render(
-    <Provider store={store}>
-        <App {...{defaultTimeStart, defaultTimeEnd}}/>
-    </Provider>, document.getElementById('root'));
+// if (process.env.NODE_ENV !== 'production') {
+//     renderCalendar('root');
+// } else {
+//     window.renderCalendar = renderCalendar;
+// }
 
-registerServiceWorker();
+renderCalendar('root');
